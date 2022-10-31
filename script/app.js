6898a84e8a13d9d3c3829eec32a9039709cd5f2a
cumptichsenne <senne.cumptich@student.howest.be>
2022-10-31 08:50:10 +0100

    added basis responsive en een listento function

--- script/app.js
+++ script/app.js
@@ -2,9 +2,10 @@ const showMovies = function (jsonObject) {
   let htmlstring_movies = '';
   for (const movies of jsonObject.data) {
     console.log(movies.title);
-    htmlstring_movies += `<div class="grid-item"><img src="${movies.cover_url}" width="300"></br>${movies.title}</div>`;
+    htmlstring_movies += `<div class="grid-item js-movie" movie_id=${movies.id}><img src="${movies.cover_url}" class="c-image"></br>${movies.title}</div>`;
   }
   document.querySelector('.js-movies').innerHTML = htmlstring_movies;
+  listenToMovies();
 };
 
 const showMovie = function (jsonObject) {
@@ -22,6 +23,17 @@ const getMovies = function () {
 const getMovie = function () {
   id = 25;
   handleData(`https://mcuapi.herokuapp.com/api/v1/movies/${id}/`, showMovie);
+};
+
+const listenToMovies = function () {
+  const buttons = document.querySelectorAll('.js-movie');
+  for (const btn of buttons) {
+    console.log('click')
+    btn.addEventListener('click', function () {
+      const id = btn.getAttribute('movie_id');
+      console.log(id)
+    });
+  }
 };
 
 const init = function () {
