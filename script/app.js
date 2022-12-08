6446e41fcfd5ac94b46169cadf3e11e907ecbc0d
cumptichsenne <senne.cumptich@student.howest.be>
2022-12-08 08:57:09 +0100

    added basic data visualisation

--- script/app.js
+++ script/app.js
@@ -17,10 +17,74 @@ const showMovie = function (jsonObject) {
                       Release date: ${jsonObject.release_date}</br>
                       Director: ${jsonObject.directed_by}
                     </h5>
-                    <img src="${jsonObject.cover_url}" class="c-image">
+                    <div class="o-layout">
+                      <div class="o-layout__item u-1-of-3">
+                        <img src="${jsonObject.cover_url}" class="c-image">
+                      </div>
+                      <div class="js-grafiek o-layout__item u-2-of-3">
+                      </div>
+                    </div>
                 </div>
             </div>`;
   document.querySelector(`.js-details`).innerHTML = htmlstring_movie;
+  ShowGrafiek(jsonObject);
+};
+
+const ShowGrafiek = function(jsonObject){
+console.log(jsonObject.box_office);
+    let Bar1 = {
+      series: [
+        {
+          name: 'Money made (in $)',
+          data: [(jsonObject.box_office/1000000).toFixed(2)],
+        },
+      ],
+      chart: {
+        type: 'bar',
+        height: 350,
+        width: 700
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: '25%',
+          endingShape: 'rounded',
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        show: true,
+        width: 2,
+        colors: ['transparent'],
+      },
+      xaxis: {
+        categories: [
+          jsonObject.title,
+        ],
+      },
+      yaxis: {
+        title: {
+          text: '$ (million)',
+        },
+      },
+      fill: {
+        opacity: 1,
+      },
+      tooltip: {
+        y: {
+          formatter: function (val) {
+            return '$ ' + val + ' million';
+          },
+        },
+      },
+    };
+    let chart = new ApexCharts(
+      document.querySelector('.js-grafiek'),
+      Bar1
+    );
+    chart.render();
 };
 
 const getMovies = function () {
